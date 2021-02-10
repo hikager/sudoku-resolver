b6adcc4f06d38b0850aa8d3070bc8d24d34080cc
Hikager <lluis22.ml@gmail.com>
2021-02-10 18:51:56 +0100

    What 	-Testing sudoku view maker (trying to show up all 9x9 elements) 	-Making from a double for to fill row-columns
    Why
    	-If I happen to do it by hand (typing it) it will be 81 elements
    	that I will need to put (element by element)

--- src/controller/sudokuController.java
+++ src/controller/sudokuController.java
@@ -10,6 +10,10 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import model.PopUpMSG;
 import model.SudokuResolver;
 
@@ -23,8 +27,14 @@ public class sudokuController implements Initializable {
     //Per mostrar errors o altres
     private PopUpMSG popUpMsg;
 
+    private int SUDOKU_PANE_WIDTH = 854;
+    private int SUDOKU_PANE_HEIGHT = 459;
+
     @FXML
-    private Button btnCloseAll;
+    private AnchorPane MainPain;
+
+    @FXML
+    private AnchorPane sudokuPane;
 
     /**
      *
@@ -34,9 +44,30 @@ public class sudokuController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        //TODO : Pass all this sudoku view maker into a class which will be called 'SudokuViewMaker'
+        //To separate each record (text-field) I decied to set a constant width and height
+        //from the SudokuView.fxml (view) sudokuPane object.
+        double lenTxt = (SUDOKU_PANE_WIDTH / 9); //Width / number of columns
+        double lenY = (SUDOKU_PANE_HEIGHT / 9); // Width / number of rows
+        for (int i = 0; i < 9; i++) {
+
+            for (int j = 0; j < 9; j++) {
+                TextField txtF = new TextField("" + 9);
+                System.out.println(lenTxt * i);
+                txtF.setLayoutX(lenTxt * i);
+                txtF.setLayoutY(lenY * j);
+                txtF.setPrefWidth(30);
+                sudokuPane.getChildren().add(txtF);
+            }
+
+        }
+
+        System.out.println("x lentgh: " + sudokuPane.widthProperty().get());
         System.out.println("HEY!");
         SudokuResolver sr = new SudokuResolver();
         System.out.println(sr.getSudokuState());
+
     }
 
 }
