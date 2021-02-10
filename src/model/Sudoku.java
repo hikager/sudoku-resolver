4c86d4c533035fa32d5448331b046eb3cdf13a41
Hikager <lluis22.ml@gmail.com>
2021-02-10 16:54:02 +0100

    What 	-Adding new features to Sudoku (pass to an abstract class) 	-Making   two classes to separet Sudoku creation and sudoku solver

--- src/model/Sudoku.java
+++ src/model/Sudoku.java
@@ -9,20 +9,47 @@ package model;
  *
  * @author LuisDAM
  */
-public class Sudoku {
+public abstract class Sudoku  {
 
-    private final int ROW_LENTGH = 9;
-    private final int COL_LENTGH = 9;
+    private final int ROW_LENTGH;
+    private final int COL_LENTGH;
 
     private int sudokuM[][];
     private boolean sudokuViewM[][];
 
+    public Sudoku(int ROW_LENTGH, int COL_LENTGH) {
+        this.ROW_LENTGH = ROW_LENTGH;
+        this.COL_LENTGH = COL_LENTGH;
+    }
+
     public Sudoku() {
-        sudokuM = new int[ROW_LENTGH][COL_LENTGH];
-        sudokuViewM = new boolean[ROW_LENTGH][COL_LENTGH];
+        this.ROW_LENTGH = 9;
+        this.COL_LENTGH = 9;
+    }
+
+    public int getROW_LENTGH() {
+        return ROW_LENTGH;
+    }
+
+    public int getCOL_LENTGH() {
+        return COL_LENTGH;
+    }
+
+    public int[][] getSudokuM() {
+        return sudokuM;
+    }
+
+    public boolean[][] getSudokuViewM() {
+        return sudokuViewM;
+    }
+
+    private void initSudoku() {
+        initSudokuMatrix();
+        initSudokuViewMatrix();
     }
 
     private void initSudokuMatrix() {
+        sudokuM = new int[ROW_LENTGH][COL_LENTGH];
         for (int row = 0; row < ROW_LENTGH; row++) {
             for (int col = 0; col < COL_LENTGH; col++) {
                 sudokuM[row][col] = 0;
@@ -31,11 +58,14 @@ public class Sudoku {
     }
 
     private void initSudokuViewMatrix() {
+        sudokuViewM = new boolean[ROW_LENTGH][COL_LENTGH];
         for (int row = 0; row < ROW_LENTGH; row++) {
             for (int col = 0; col < COL_LENTGH; col++) {
                 sudokuViewM[row][col] = false;
             }
         }
     }
+    
+   
 
 }
